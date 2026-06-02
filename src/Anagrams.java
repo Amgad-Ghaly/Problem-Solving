405a141966ab388d64e70e2cf81771df127fb59e
Amgad-Ghaly <amgad.n.ghaly@gmail.com>
2026-06-02 12:58:13 +0300

    solve anagrams problem

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class Anagrams {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String a = input.nextLine();
+        String b = input.nextLine();
+        boolean c =isAnagram(a,b);
+        if(c ){
+            System.out.println("Anagrams");
+        }
+        else{
+            System.out.println("Not Anagrams");
+        }
+    }
+
+    static boolean isAnagram(String a, String b) {
+    char []arr1 = a.toCharArray();
+    char []arr2 = b.toCharArray();
+    // the main idea
+    Arrays.sort(arr1);
+    Arrays.sort(arr2);
+    if(Arrays.equals(arr1,arr2)){
+        return true;
+    }
+    else {
+        return false;
+    }
+
+
+    }
 }
